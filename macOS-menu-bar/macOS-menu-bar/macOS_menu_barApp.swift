a19ea0baa8fbe6d14ee903fef1b967c440982645
Wills Liou <willsliou@protonmail.com>
2021-07-05 15:35:20 -0700

    Status Button Toggle

--- macOS-menu-bar/macOS-menu-bar/macOS_menu_barApp.swift
+++ macOS-menu-bar/macOS-menu-bar/macOS_menu_barApp.swift
@@ -15,7 +15,7 @@ struct macOS_menu_barApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
+        } // WindowGroup
     }
 }
 
@@ -29,10 +29,46 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
     func applicationDidFinishLaunching(_ notification: Notification) {
         let menuView = MenuView()
-    }
-
+        
+        // create popover
+        popOver.behavior = .transient
+        popOver.animates = true
+        // Setting Empty View Controller
+        // https://developer.apple.com/documentation/appkit/nswindow/1419615-contentviewcontroller
+        popOver.contentViewController = NSViewController()
+        popOver.contentViewController?.view = NSHostingView(rootView: menuView)
+        
+        // Status Bar Button
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        
+        // Validation check if status button is avaliable
+        if let MenuButton = statusItem?.button {
+            
+            MenuButton.image = NSImage(systemSymbolName: "icloud.and.arrow.up.fill", accessibilityDescription: nil)
+            MenuButton.action = #selector(MenuButtonToggle)
+        } // if MenuButton bracket
+    } // function application bracket
     
-}
+    
+    
+    
+    
+    
+    @objc func MenuButtonToggle() {
+        if let menuButton = statusItem?.bottom{
+//             Top Get
+            self.popOver.show(relativeTo: menuButton.bounds, of: menuButton, preferredEdge: NSRectEdge.minY)
+            
+        }
+    }
+    
+    
+} // class object
+    
+    
+    
+    
 
 
 // Pop Over Menu
